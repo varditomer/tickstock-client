ba3df0984a750f82dcc7397096a3122f368790c4
varditomer <tomer.vardi@gmail.com>
2025-05-30 13:34:21 +0300

    feat: add TickStock logo to header and update styling for layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
+import tickstockLogo from "../assets/images/tickstock_logo.png";
+
 export const Header = () => {
   return (
     <header>
-      <h1>📈 TickStock</h1>
+      <h1>
+        <img
+          src={tickstockLogo}
+          alt="TickStock Logo"
+          //   style="height:1em;vertical-align:middle;"
+        />{" "}
+        TickStock
+      </h1>
     </header>
   );
 };
